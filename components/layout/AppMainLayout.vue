29c0ae682877d63e3d866f8e908944ec9e5211ab
hxbk/wangchao <1174067295@qq.com>
2026-05-13 13:44:54 +0800

    fix(layout): 移除AppMainLayout中的多余边框样式
    - 移除了header容器的底部边框样式(border-b border-slate-100)
    - 保留了原有的内边距和定位样式
    - 优化了页面布局的视觉效果

--- components/layout/AppMainLayout.vue
+++ components/layout/AppMainLayout.vue
@@ -9,7 +9,7 @@ import AppSidebar from '@/components/layout/AppSidebar.vue'
       <AppSidebar />
 
       <div class="min-w-0  border border-slate-200/80 bg-white/90 shadow-[0_18px_50px_rgba(15,23,42,0.06)] relative rounded-xl">
-        <div class="border-b border-slate-100 px-5 pb-4 pt-5 sm:px-5 lg:px-6 absolute  z-10 right-0">
+        <div class=" px-5 pb-4 pt-5 sm:px-5 lg:px-6 absolute  z-10 right-0">
           <AppHeader />
         </div>
 
